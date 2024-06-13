5ff2fe4a57280a690206cf4337eeac19bcac51c6
Serveladik <vladyslav.sabadakh@litslink.com>
2024-06-13 19:16:14 +0300

    Part solution2

--- app/main.py
+++ app/main.py
@@ -14,3 +14,16 @@ class CarWashStation:
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
+
+    def serve_cars(self, cars: list) -> list:
+        serve_filter_list = []
+        for car in cars:
+            if car.clean_mark < self.clean_power:
+                car.clean_mark = 0
+                serve_filter_list.append(car)
+        return serve_filter_list
+
+    def calculate_washing_price (self, cars: list) -> int:
+        for car in cars:
+            if car.comfort_class * (self.clean_power - car.clean_mark) * self.average_rating / self.distance_from_city_center:
+
