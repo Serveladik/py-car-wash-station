3a9aaabefd3973db4818017ea3e83374ad858c95
Serveladik <vladyslav.sabadakh@litslink.com>
2024-06-13 19:29:45 +0300

    Part solution3

--- app/main.py
+++ app/main.py
@@ -6,9 +6,7 @@ class Car:
         self.brand = brand
 
 
-
 class CarWashStation:
-    # write your code here
     def __init__(self, distance_from_city_center: float, clean_power: int, average_rating: float, count_of_ratings: int):
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
@@ -18,12 +16,20 @@ class CarWashStation:
     def serve_cars(self, cars: list) -> list:
         serve_filter_list = []
         for car in cars:
-            if car.clean_mark < self.clean_power:
-                car.clean_mark = 0
+            if car.clean_mark <= self.clean_power:
+                car.clean_mark = self.clean_power
                 serve_filter_list.append(car)
         return serve_filter_list
 
-    def calculate_washing_price (self, cars: list) -> int:
-        for car in cars:
-            if car.comfort_class * (self.clean_power - car.clean_mark) * self.average_rating / self.distance_from_city_center:
+    def calculate_washing_price (self, car) -> int:
+        return round(car.comfort_class * (self.clean_power - car.clean_mark) * self.average_rating
+                     / self.distance_from_city_center, 1)
 
+    def wash_single_car (self, car):
+        if self.clean_power >= car.clean_mark:
+            car.clean_mark = self.clean_power
+
+    def rate_service(self, new_rating: float):
+        current_total_rating = self.average_rating * self.count_of_ratings
+        self.count_of_ratings += 1
+        self.average_rating = round((current_total_rating + new_rating) / self.count_of_ratings, 1)
