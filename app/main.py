40319e6df01256f6f2e295fd52d71321ae67981d
Serveladik <vladyslav.sabadakh@litslink.com>
2024-06-13 19:41:31 +0300

    SolutionFix

--- app/main.py
+++ app/main.py
@@ -1,12 +1,14 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str):
+    def __init__(self, comfort_class: int, clean_mark: int,
+                 brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: float, clean_power: int, average_rating: float, count_of_ratings: int):
+    def __init__(self, distance_from_city_center: float, clean_power: int,
+                 average_rating: float, count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
@@ -23,15 +25,16 @@ class CarWashStation:
                 self.wash_single_car(car)
         return income
 
-    def calculate_washing_price (self, car) -> int:
-        return round(car.comfort_class * (self.clean_power - car.clean_mark) * self.average_rating
-                     / self.distance_from_city_center, 1)
+    def calculate_washing_price(self, car: Car) -> float:
+        return round(car.comfort_class * (self.clean_power - car.clean_mark)
+                     * self.average_rating / self.distance_from_city_center, 1)
 
-    def wash_single_car (self, car):
+    def wash_single_car(self, car: Car) -> None:
         if self.clean_power >= car.clean_mark:
             car.clean_mark = self.clean_power
 
-    def rate_service(self, new_rating: float):
+    def rate_service(self, new_rating: float) -> None:
         current_total_rating = self.average_rating * self.count_of_ratings
         self.count_of_ratings += 1
-        self.average_rating = round((current_total_rating + new_rating) / self.count_of_ratings, 1)
+        self.average_rating = round((current_total_rating + new_rating)
+                                    / self.count_of_ratings, 1)
